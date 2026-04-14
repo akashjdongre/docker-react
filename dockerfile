fbacb3f0fddb64401cdd5a11b0d372b8a6380d34
akash j dongre <akashjdongre@gmail.com>
2026-04-14 12:57:46 +0530

    yml file : port added

--- dockerfile
+++ dockerfile
@@ -4,6 +4,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 CMD ["npm", "run", "build"]
-
-FROM nginx:alpine
+EXPOSE 80
+FROM nginx
 COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h4>Hello , Akash Dongre !<br/> Welcome to React App</h4>
+          <h4>Hello , Akash Dongre !<br/> Welcome to React App : AWS Hosting</h4>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
